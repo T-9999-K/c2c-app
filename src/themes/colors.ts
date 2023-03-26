cea2a410f09c82fbb41e029b115105c9a16a77d7
tak_kojima <takuto9119@gmail.com>
2023-03-26 18:00:26 +0900

    add storybook

---
+++ src/themes/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+  primary: '#3 f 51 b 5',
+  primaryDark: '#2 c 387 e',
+  primaryLight: '#6573 c 3',
+  secondary: '#f 50057',
+  secondaryDark: '#ab 003 c',
+  secondaryLight: '#f 73378',
+  border: '#cdced 2',
+  danger: '#ed 1 c 24',
+  gray: '#6 b 6 b 6 b',
+  black: '#000000',
+  white: '#ffffff',
+}
+
+export default colors
